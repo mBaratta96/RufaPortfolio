7f8d1f884bf3e19e637588b9e0cb48deeaac06ea
Marco Baratta <marcobaratta96@gmail.com>
2022-03-22 17:55:11 +0100

    first commit

---
+++ src/components/Preview/Preview.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Preview.less";
+
+const Preview = () => {
+	return <div className="Preview-root">Hello</div>;
+};
+
+export default Preview;
---
+++ src/components/Slide/Slide.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Slide.less";
+import Preview from "../Preview";
+
+const Slide = () => {
+	return (
+		<div className="Slide-root">
+			<Preview />
+			{"Hello"}
+		</div>
+	);
+};
+
+export default Slide;
