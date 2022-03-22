e06289e67be5df5a0bfe7926df5c1db026958932
Marco Baratta <marcobaratta96@gmail.com>
2022-03-22 18:30:57 +0100

    images

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Preview.less";
 
-const Preview = () => {
-	return <div className="Preview-root">Hello</div>;
+type imageArray = Array<string>;
+
+const imageUrls: imageArray = [
+	"planet",
+	"airbaloon",
+	"beach",
+	"gradient",
+	"paint",
+];
+
+const Preview = (): JSX.Element => {
+	const previewContent = imageUrls.map(
+		(url: string, index: number): JSX.Element => {
+			return (
+				<div key={index}>
+					<img src={new URL("./beach.jpg", import.meta.url).href}></img>
+				</div>
+			);
+		}
+	);
+	return <div className="Preview-root">{previewContent}</div>;
 };
 
 export default Preview;
