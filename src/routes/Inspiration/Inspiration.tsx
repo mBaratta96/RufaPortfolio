8eda33468ae5406662bde60d797ee36a8dbc01c2
Marco Baratta <marcobaratta96@gmail.com>
2022-04-11 17:00:14 +0200

    inspiration

--- src/routes/Inspiration/Inspiration.tsx
+++ src/routes/Inspiration/Inspiration.tsx
@@ -2,16 +2,16 @@ import React, { Fragment } from "react";
 import Slide from "../../components/Slide";
 
 const images = [
+	"aparajito",
+	"barryLyndon",
+	"brazil",
+	"heavensGate",
 	"immortalStory",
+	"journeyItaly",
 	"sacrifice",
-	"ninthConfiguration",
-	"immortalStory",
-	"immortalStory",
-	"immortalStory",
-	"immortalStory",
-	"immortalStory",
-	"immortalStory",
-	"immortalStory",
+	"sevenSamurai",
+	"silverGlobe",
+	"womanUnderInfluence",
 ].map((title) => {
 	return {
 		slide: new URL(`./images/${title}Slide.webp`, import.meta.url).href,
